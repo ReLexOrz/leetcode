818e818256483cad10e4547f5c0952679cdfe900
relexorz <relexorz@qq.com>
2020-02-24 18:56:10 +0800

    #11 解答

--- main.go
+++ main.go
@@ -21,10 +21,11 @@ func main() {
 	funcMap[8] = logic.TestMyAtoi
 	funcMap[9] = logic.TestIsPalindrome
 	funcMap[10] = logic.TeatIsMatch
+	funcMap[11] = logic.TestMaxArea
 	funcMap[15] = logic.TestThreeSum
 
 	//输入你想运行的测试题编号即可返回测试用例
-	testNum := 10
+	testNum := 11
 	fn, ok := funcMap[testNum]
 	if ok {
 		fn()
