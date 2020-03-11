380fe67d081c58d4c89bffd21180940f00e69069
relexorz <relexorz@qq.com>
2020-03-11 10:28:28 +0800

    #25解法 未完成

--- logic/t24_swapPairs.go
+++ logic/t24_swapPairs.go
@@ -5,13 +5,9 @@ import "fmt"
 /*
 #24 两两交换链表中的节点
 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
-
 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
 
-
-
 示例:
-
 给定 1->2->3->4, 你应该返回 2->1->4->3.
 
 来源：力扣（LeetCode）
@@ -41,10 +37,10 @@ func TestSwapPairs() {
 		Val:  1,
 		Next: lB,
 	}
-	fmt.Println("#24 TestAddTwoNumbers Input:")
+	fmt.Println("#24 TestSwapPairs Input:")
 	PrintListNode(lA)
 	res := swapPairs(lA)
-	fmt.Println("#24 TestAddTwoNumbers Res:")
+	fmt.Println("#24 TestSwapPairs Res:")
 	PrintListNode(res)
 }
 
--- main.go
+++ main.go
@@ -36,11 +36,12 @@ func main() {
 	funcMap["#22"] = logic.TestGenerateParenthesis
 	funcMap["#23"] = logic.TestMergeKLists
 	funcMap["#24"] = logic.TestSwapPairs
+	funcMap["#25"] = logic.TestReverseKGroup
 	//面试题
 	funcMap["*57_2"] = logic.TestFindContinuousSequence
 
 	//输入你想运行的测试题编号即可返回测试用例
-	testStr := "#24"
+	testStr := "#25"
 	fn, ok := funcMap[testStr]
 	if ok {
 		fn()
